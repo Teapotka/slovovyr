20003bd9da247399ed6f422f2b309ab699bacbee
Tymofii <tymofii.sukhariev@gmail.com>
2022-08-11 21:54:14 +0300

    trying to add cors

--- components/Content/Game/Keyboard/Keyboard.tsx
+++ components/Content/Game/Keyboard/Keyboard.tsx
@@ -5,6 +5,7 @@ const Keyboard = () => {
     const keys = ['й','ц','у','к','е','н','г','ш','щ','з','х','ї',
                   'ф','і','в','а','п','р','о','л','д','ж','є',<Arrow/>,
                   'ґ','я','ч','с','м','и','т','ь','б','ю']
+                  //TODO: Cors, Animejs, Cashe, Data storage
   return (
     <div className='keyboard'>
         {keys.map((k)=><div key={keys.indexOf(k)}>{k}</div>)}
--- pages/api/[...word].ts
+++ pages/api/[...word].ts
@@ -1,38 +1,65 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
+import NextCors from 'nextjs-cors';
 import axios from 'axios'
 import * as cheerio from 'cheerio'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+// const cors = Cors({
+//   methods: ['POST', 'GET', 'HEAD'],
+//   origin: '*'
+// })
 
-    const input = req.query.word!
-    const finder = input[0]
-    const index = input.length == 2 ? input[1] : 0
-    const url = encodeURI(`https://slovnyk.ua/index.php?swrd=${finder}`)
-    let length = 0
-  
-    console.log(url)
-    axios.get(url)
-      .then((d) => cheerio.load(d.data, { decodeEntities: true }))
-      .then(($) => {
-        const html = $('div.toggle-sum p')
-        length = html.length
+// // Helper method to wait for a middleware to execute before continuing
+// // And to throw an error when an error happens in a middleware
+// function runMiddleware(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+//   fn: Function
+// ) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result: any) => {
+//       if (result instanceof Error) {
+//         return reject(result)
+//       }
+//       console.log('cors')
 
-        if (length == 0)
-          return { exsisting: false }
+//       return resolve(result)
+//     })
+//   })
+// }
 
-        if (index >= length)
-          return 'out of range'
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await NextCors(req, res, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',    
+  });
+  // await runMiddleware(req, res, cors)
+  const input = req.query.word!
+  const finder = input[0]
+  const index = input.length == 2 ? input[1] : 0
+  const url = encodeURI(`https://slovnyk.ua/index.php?swrd=${finder}`)
+  let length = 0
+  console.log(url)
+  axios.get(url)
+    .then((d) => cheerio.load(d.data, { decodeEntities: true }))
+    .then(($) => {
+      const html = $('div.toggle-sum p')
+      length = html.length
 
+      if (length == 0)
+        return { exsisting: false }
 
-        return {
-          exsisting: true,
-          count: length,
-          word: {
-            index: index,
-            info: html.eq(+index).text()
-          }
+      if (index >= length)
+        return 'out of range'
+
+      return {
+        exsisting: true,
+        count: length,
+        word: {
+          index: index,
+          info: html.eq(+index).text()
         }
-      })
-      .then((d) => res.json(d))
-      .catch(()=> res.send(`something went wrong`))
+      }
+    })
+    .then((d) => { res.status(200).json(d) })
+    .catch(() => res.send(`something went wrong`))
 }
