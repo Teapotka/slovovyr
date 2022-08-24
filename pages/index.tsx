b9d6d44a6f0939facf742cf3d59de5a0e38e6a3a
Tymofii <tymofii.sukhariev@gmail.com>
2022-08-25 00:26:36 +0300

    bug check

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import type { NextPage } from 'next'
 import Landing from '../components/Content/Landing/Landing'
 import GridTemp from '../components/Grid/GridTemp'
 import HeaderTemp from '../components/Header/HeaderTemp'
+import words from '../data/words'
 
 const Home: NextPage = () => {
+  console.log('WORD',  words
+  .filter(w => w.region == 'center')
+  [0].words[new Date().getDate() - 1], new Date().getDate() - 1, words
+  .filter(w => w.region == 'center'))
   return (
    <>
    <GridTemp state='is-scrolless'>
