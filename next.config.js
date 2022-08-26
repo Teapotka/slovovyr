5375a3aeafe76bd750794c9993fa4121dbbed67c
Tymofii <tymofii.sukhariev@gmail.com>
2022-08-26 16:52:10 +0300

    open Graph trying to fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers:[
+    {
+      key: 'X-Robots-Tag',
+      value: 'index,follow',
+    },
+  ],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
