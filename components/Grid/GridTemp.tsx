d536e887b81c9647d3645728f57cc5d575bdaa12
Tymofii <tymofii.sukhariev@gmail.com>
2022-08-30 14:07:06 +0300

    fixing addressbar problem

--- components/Grid/GridTemp.tsx
+++ components/Grid/GridTemp.tsx
@@ -3,14 +3,12 @@ import { loadThemeData } from '../../data/localMemory'
 import ModalContainer from '../Modals/ModalContainer'
 
 const GridTemp:FC<{children: ReactNode[], state : string}> = ({children, state}) => {
-   const [addressbar, setbar] = useState<null|number>(null)
     useEffect(()=>{
       loadThemeData()
-      setbar( document.querySelector('.temp-l-grid')!.clientHeight - window.innerHeight + 50)
     },[])
     return (
       <>
-      <div className={`temp-l-template temp-l-grid ${state}`} style={ {gridTemplateRows:`50px calc(100vh - ${addressbar == null ? 50 : addressbar}px)`}} >
+      <div className={`temp-l-template temp-l-grid ${state}`} >
       {children}
       </div>
       <ModalContainer/>
