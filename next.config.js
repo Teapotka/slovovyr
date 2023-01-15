e2f328fabb6cf54acaa313db48df34ce7d826c6b
Teapotka <kalyvanivan33@gmail.com>
2023-01-15 15:24:23 +0100

    remove eslint during build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   env:{
     BASE_URL: process.env.BASE_URL,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
